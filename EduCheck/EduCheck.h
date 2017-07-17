a63e40a70e40304d7c74049d4eeaece6f2780f61
Theodore Strauss <theodorestrauss@Theodores-MacBook-Pro.local>
2017-07-16 22:13:41 -0400

    Created project

---
+++ EduCheck/EduCheck.h
@@ -0,0 +1,19 @@
+//
+//  EduCheck.h
+//  EduCheck
+//
+//  Created by Theodore Strauss on 7/16/17.
+//  Copyright © 2017 Theodore Strauss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EduCheck.
+FOUNDATION_EXPORT double EduCheckVersionNumber;
+
+//! Project version string for EduCheck.
+FOUNDATION_EXPORT const unsigned char EduCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EduCheck/PublicHeader.h>
+
+
